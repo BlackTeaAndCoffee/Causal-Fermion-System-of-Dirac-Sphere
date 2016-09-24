c3df0114fdf378af352aab798ae424b9f94e959c
mustafa akman <mustafaakman@ymail.com>
2016-09-24 23:55:53 +0200

    Commit vor Zweigung

--- ErstesProgramm.py
+++ ErstesProgramm.py
@@ -3,11 +3,28 @@ from sympy import *
 from sympy.utilities.lambdify import lambdify
 from sympy.abc import r,x
 from sympy.physics.quantum import TensorProduct
+from pyx import *
+from pyx.graph import axis
 import scipy.special
 import scipy.misc
 import numpy as np
 import quartic as qp
 import DiagXY as db
+
+
+def Diag_plot(x_values, y_matrix, X_Title, Y_Title, PDFTitle):
+
+
+
+    c = graph.graphxy(width=10,height=10,
+                      x = graph.axis.linear(min = min(x_values), max = max(x_values), title= X_Title),
+                      y = graph.axis.linear(min = np.amin(y_matrix), max = np.amax(y_matrix), title= Y_Title))
+
+    for i in range(np.shape(y_Matrix)[0]):
+        y_values = y_matrix[i,:]
+        c.plot(graph.data.values(x = x_values, y = y_values),[graph.style.line([color.rgb.blue])])
+
+    c.writePDFfile(PDFTitle)
 
 def Prefactor(n):
     return int(scipy.misc.factorial(n + 2))/(8 * pi**(2)*int(scipy.misc.factorial(n)))
@@ -16,14 +33,16 @@ def DiracEigenvalue(n):
     return (2*n + 1)/2
 
 def IntegralKernelPlus(n, r, theta, phi):
-    lala = jacobi(n, 1/2, 3/2, r)
-    return Prefactor(n)*(cos(r/2)*lala*ident -
-                        1j*sin(r/2)*lala*sigma_r(theta, phi))
+    lala1 = jacobi(n, 1/2, 3/2, r)
+    lala2 = jacobi(n, 3/2, 1/2, r)
+    return Prefactor(n)*(cos(r/2)*lala1*ident -
+                        1j*sin(r/2)*lala2*sigma_r(theta, phi))
 
 def IntegralKernelMinus(n, r, theta, phi):
-    lala = jacobi(n , 1/2, 3/2, r)
-    return Prefactor(n)*(cos(r/2)*lala*ident +
-                        1j*sin(r/2)*lala*sigma_r(theta, phi))
+    lala1 = jacobi(n, 1/2, 3/2, r)
+    lala2 = jacobi(n, 3/2, 1/2, r)
+    return Prefactor(n)*(cos(r/2)*lala1*ident +
+                        1j*sin(r/2)*lala2*sigma_r(theta, phi))
 
 def sigma_r(theta, phi):
     return sin(theta)*cos(phi)*sigma1 + sin(theta)*sin(phi)*sigma2 + cos(theta)*sigma3
@@ -63,12 +82,27 @@ def Lagrangian_without_Max(t, r, theta, phi,N):
     sub = ClosedChain(t, r, theta, phi, N)
     return trace(sub*sub) - 0.25 * trace(sub)**2
 
+def Lagrangian_without_Max2(t, r, theta, phi,N, kappa):
+    sub = ClosedChain(t, r, theta, phi, N)
+    return trace(sub*sub) +(kappa - 0.25) * trace(sub)**2
+
 def TraceConstraint(N):
     add = 0
     for n in range(1, N+1):
         add += rho[n-1]*(DiracEigenvalue(n)**2 - 0.25)
     return add
 
+def Lagrange_Schar(a, x_values, y_matrix, kappa):
+    for m,k_value in enumerate(k_list):
+        k = [k_value]
+        a = simplify(ClosedChain(t, r, 0, 0, N))
+        #lagr = lambdify((t,r), simplify(Lagrangian_without_Max(t,r,0,0,N)))
+        lagr = lambdify((t,r), simplify( Lagrangian_without_Max2( t, r, 0,0,N, kappa)))
+        for j,point in enumerate(x_values):
+            yolo = np.sin(point)**2*lagr(t,point)
+
+            y_Matrix[m,j]=yolo
+    Diag_plot(list(x_values), y_Matrix, 'Radius', 'Lagr', 'N=1_LAgr(r)_kappa=%0.2f'%kappa)
 
 
 if __name__ == "__main__":
@@ -77,33 +111,45 @@ if __name__ == "__main__":
     sigma2 = Matrix([[0, -1j],[1j, 0]])
     sigma3 = Matrix([[1, 0],[0, -1]])
     ident =  Matrix([[1,0],[0,1]])
-    k = [10]
+    #k = [10]
+
+    rho1, k1 = symbols('rho1 k1')
     w = [0]
     N = 1
-    rho = [N]
+    rho = [rho1]
     #simplify(ClosedChain(t, r, 0, 0, N))
 
     a1, a2, a3, a4, a5  = symbols('a1 a2 a3 a4 a5')
 
-
-    a = simplify(ClosedChain(t, r, 0, 0, N))
-
-    print simplify(Lagrangian_without_Max(t,r,0,0,N))
-    lagr = lambdify((t,r), simplify(Lagrangian_without_Max(t,r,0,0,N)))
-
     L = 100
-    x_values= []
-    y_values =[]
-    for i in range(L):
-        ter = i*np.pi/L
-        x_values.append(ter)
-        yolo = np.sin(ter)**2*lagr(t,ter)
-        print(yolo)
-        y_values.append(yolo)
+    k=[k1]
 
-    db.f_gegn_x(list(x_values), y_values, 'Radius', 'Lagr', 'N=1_LAgr(r)')
+    K_Anzahl=10
+
+    k_list=np.linspace(0.3,0.5,K_Anzahl)
+    a = ClosedChain(t, r, 0, 0, N)
+
+    #print simplify(Lagrangian_without_Max(t,r,0,0,N))
+    #lagr = lambdify((t,r), simplify(Lagrangian_without_Max(t,r,0,0,N)))
+
+    dd =  (trace(a)**2 + trace(a*a)**2)/2
+    print simplify(dd)
+    #ddl = lambdify((t,r), dd)
+    kappa = 0.1
+
+    x_values= np.linspace(0,np.pi,L)
+    y_Matrix =np.zeros((K_Anzahl, L))
+    y_values=[]
+#    for i in range(L):
+#        y_values.append(ddl(t,x_values[i]))
+
+#    db.f_gegn_x(list(x_values), y_values, 'radius', 'Lagr', 'test')
+
+    #Lagrange_Schar(a, x_values, y_Matrix, kappa)
+
     #Nullstellen = qp.x1plus(*c)
     #print Nullstellen
     #print simplify(Nullstellen)
     #print roots(simplify(a.charpoly(x).as_expr()),x)
+
 
